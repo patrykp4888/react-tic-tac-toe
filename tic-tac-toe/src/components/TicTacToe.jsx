79d96eeffd1a28f39c324350f19d720501ee07a9
Patryk Partyka <136442729+patrykpartyka@users.noreply.github.com>
2023-12-17 20:46:22 +0100

    strike-logic-ready

--- tic-tac-toe/src/components/TicTacToe.jsx
+++ tic-tac-toe/src/components/TicTacToe.jsx
@@ -4,8 +4,30 @@ import Board from "./Board";
 const PLAYER_X = "X";
 const PLAYER_O = "O";
 
-function checkWinner(){
-    console.log("check winner")
+const winningCombinations = [
+    //Rows
+    {combo: [0,1,2], strikeClass: "strike-row-1"},
+    {combo: [3,4,5], strikeClass: "strike-row-2"},
+    {combo: [6,7,8], strikeClass: "strike-row-3"},
+    //Columns
+    {combo: [0,3,6], strikeClass: "strike-column-1"},
+    {combo: [1,4,7], strikeClass: "strike-column-2"},
+    {combo: [2,5,8], strikeClass: "strike-column-3"},
+    //Diagonals
+    {combo: [0,4,8], strikeClass: "strike-diagonal-1"},
+    {combo: [2,4,6], strikeClass: "strike-diagonal-2"}
+];
+
+function checkWinner(tiles, setStrikeClass) {
+    for(const {combo, strikeClass} of winningCombinations){
+        const tileValue1 = tiles[combo[0]]
+        const tileValue2 = tiles[combo[1]]
+        const tileValue3 = tiles[combo[2]]
+
+        if(tileValue1 !== null && tileValue1 === tileValue2 && tileValue1 === tileValue3){
+            setStrikeClass(strikeClass)
+        }
+    }
 }
 
 function TicTacToe(){
@@ -28,7 +50,7 @@ function TicTacToe(){
     }
 
     useEffect(()=>{
-        checkWinner();
+        checkWinner(tiles, setStrikeClass);
     }, [tiles]);
 
     return (
