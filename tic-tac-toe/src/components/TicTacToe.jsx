f5ed1bb8601e64ae901fb7959debff8edd7cc581
Patryk Partyka <136442729+patrykpartyka@users.noreply.github.com>
2023-12-17 19:12:32 +0100

    x-o-hover-effect-before-player-click-added

--- tic-tac-toe/src/components/TicTacToe.jsx
+++ tic-tac-toe/src/components/TicTacToe.jsx
@@ -25,7 +25,7 @@ function TicTacToe(){
     return (
         <div>
             <h1>Tic Tac Toe</h1>
-            <Board tiles={tiles} onTileClick={handleTileClick}/>
+            <Board playerTurn={playerTurn} tiles={tiles} onTileClick={handleTileClick}/>
         </div>
     );
 }
