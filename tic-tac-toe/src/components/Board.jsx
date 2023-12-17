0877eb826a9194f9793fca044a0485fbd5c511a3
Patryk Partyka <136442729+patrykpartyka@users.noreply.github.com>
2023-12-17 18:21:13 +0100

    grid-borders-css-added

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -3,15 +3,15 @@ import Tile from "./Tile";
 
 function Board() {
     return <div className="board">
-        <Tile/>
-        <Tile/>
-        <Tile/>
-        <Tile/>
-        <Tile/>
-        <Tile/>
-        <Tile/>
-        <Tile/>
-        <Tile/>
+        <Tile className='right-border bottom-border'/>
+        <Tile className='right-border bottom-border'/>
+        <Tile className='bottom-border'/>
+        <Tile className='right-border bottom-border'/>
+        <Tile className='right-border bottom-border'/>
+        <Tile className='bottom-border'/>
+        <Tile className='right-border'/>
+        <Tile className='right-border'/>
+        <Tile />
         <Strike/>
     </div>;
 }
